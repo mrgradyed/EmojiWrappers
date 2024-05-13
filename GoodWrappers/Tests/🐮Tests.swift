ab697b55f98888db99f72cbbe114ca82c3713627
Emiliano D'Alterio <emiliano@everli.com>
2024-05-14 01:14:45 +0200

    add equality tests

--- GoodWrappers/Tests/🐮Tests.swift
+++ GoodWrappers/Tests/🐮Tests.swift
@@ -4,23 +4,38 @@ import GoodWrappers
 import XCTest
 
 class 🐮Tests: XCTestCase {
-    struct Custom {
+    struct Custom: Equatable {
         var props: String = "123"
         var propi: Int = 456
     }
 
-    func testRead🐮() {
+    func testRead() {
         let cow = 🐮(wrappedValue: Custom())
         let cowCopy = cow // copy "cowed" value type
 
         XCTAssert(cow.wrapper === cowCopy.wrapper) // both pointing to the same wrapper class
     }
 
-    func testWrite🐮() {
+    func testWrite() {
         let cow = 🐮(wrappedValue: Custom())
         var cowCopy = cow // copy "cowed" value type
         cowCopy.wrappedValue = .init() // set new value type
 
         XCTAssert(cow.wrapper !== cowCopy.wrapper) // different wrapper instances
+    }
+
+    func testEqual() {
+        let cow = 🐮(wrappedValue: Custom())
+        let cowCopy = cow // copy "cowed" value type
+
+        XCTAssert(cow.wrappedValue == cowCopy.wrappedValue)
+    }
+
+    func testEquality() {
+        struct Equality: Equatable {
+            @🐮 var props: String = "123"
+        }
+
+        XCTAssert(Equality() == Equality())
     }
 }
