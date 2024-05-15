0f289cbca34d10215050dfde0befe343e861f3a8
Emiliano D'Alterio <emiliano@everli.com>
2024-05-15 23:07:23 +0200

    add unit test for 🐮 Codable conformance

--- EmojiWrappers/Tests/🐮Tests.swift
+++ EmojiWrappers/Tests/🐮Tests.swift
@@ -4,7 +4,7 @@ import Foundation
 import XCTest
 
 class 🐮Tests: XCTestCase {
-    struct TestStruct: Equatable {
+    struct TestStruct: Equatable, Codable {
         var propA: String = "123"
         var propB: Int = 456
     }
@@ -51,17 +51,45 @@ class 🐮Tests: XCTestCase {
     }
 
     func testWrappedEquality() {
-        DispatchQueue.main.async {
-            struct Wrapped: Equatable {
-                @🐮 var propC: TestStruct = .init()
-            }
-
-            let a = Wrapped()
-            var b = a
-
-            XCTAssertTrue(a == b)
-            b.propC = .init()
-            XCTAssertFalse(a != b)
+        struct Wrapped: Equatable {
+            @🐮 var propC: TestStruct = .init()
         }
+
+        let a = Wrapped()
+        var b = a
+
+        XCTAssertTrue(a == b)
+
+        b.propC = .init()
+
+        XCTAssertFalse(a != b)
+    }
+
+    func testWrappedDecodable() {
+        struct Wrapped: Equatable, Decodable {
+            @🐮 var propC: TestStruct = .init()
+        }
+
+        let json = """
+        { "propC": { "propA" : "propA", "propB" : 100 } }
+        """
+
+        let wrapped = try! JSONDecoder().decode(Wrapped.self, from: json.data(using: .utf8)!)
+
+        XCTAssertTrue(wrapped.propC == .init(propA: "propA", propB: 100))
+    }
+
+    func testWrappedEncodable() {
+        struct Wrapped: Equatable, Encodable {
+            @🐮 var propC: TestStruct = .init()
+        }
+
+        let wrapped = Wrapped(propC: .init(propA: "propA", propB: 100))
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = [.sortedKeys]
+        let encodedData = try! encoder.encode(wrapped)
+        let encodedString = String(data: encodedData, encoding: .utf8)
+
+        XCTAssertTrue(encodedString == "{\"propC\":{\"propA\":\"propA\",\"propB\":100}}")
     }
 }
